484c741ef1f4369fa0f5232e5f40bce7cc1f2475
Felix Erkinger <wuxxin@gmail.com>
2024-04-03 03:14:02 +0200

    renamed

--- scripts/shell_inside_sim.sh
+++ scripts/shell_inside_sim.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+set -eo pipefail
+# set -x
 current="$(sudo virsh list --id --state-running | sort |uniq | grep -v "^$" | head -n 1)"
 if test -n "$current"; then 
     addr="$(sudo virsh guestinfo "$current"| grep "if.1.addr.0.addr" | sed -r "s/[^:]+: (.+)/\1/g" )"
