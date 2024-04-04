3a5a5c5809304ddc6fda5f0f216301ad8fdc49e1
Felix Erkinger <wuxxin@gmail.com>
2024-04-04 16:00:24 +0200

    update ascii fishes

--- examples/safe/compose/hello-compose/backend/main.go
+++ examples/safe/compose/hello-compose/backend/main.go
@@ -9,20 +9,27 @@ import (
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(r.URL.RawQuery)
 	fmt.Fprintf(w, `
+<!DOCTYPE html>
+<html>
+<head>
+<style>pre { font-family: monospace; font-size: 2em; white-space: pre; }</style>
+</head>
+<body>
+<pre>
 	><((((°>
-
 	              ><((((°>
 
 		><((((°>
 
   <°)))><	         ><((((°>
-
               <°)))><
 
         <°)))><         <')))))><
 
-Hello from a Compose Container!
-
+    Hello from a Compose Container!
+</pre>
+</body>
+</html>
 `)
 }
 
