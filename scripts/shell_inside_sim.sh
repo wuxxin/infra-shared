a0c58bb28ca1abc21fd9e058ac920ac5ee5ec8a8
Felix Erkinger <wuxxin@gmail.com>
2024-06-16 00:42:47 +0200

    also activate agent per default into sim

--- scripts/shell_inside_sim.sh
+++ scripts/shell_inside_sim.sh
@@ -4,5 +4,5 @@ set -eo pipefail
 current="$(sudo virsh list --id --state-running | sort |uniq | grep -v "^$" | head -n 1)"
 if test -n "$current"; then 
     addr="$(sudo virsh guestinfo "$current"| grep "if.1.addr.0.addr" | sed -r "s/[^:]+: (.+)/\1/g" )"
-    ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "core@$addr"
+    ssh -A -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "core@$addr"
 fi
