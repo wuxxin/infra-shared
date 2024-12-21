ed858d4bf0062ffcf9333951e45a90c7df44d855
Felix Erkinger <wuxxin@gmail.com>
2024-12-21 02:41:29 +0100

    write_removeable also works for non empty drives now

--- scripts/write_removeable.py
+++ scripts/write_removeable.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
-"""Write image from image_path to removable storage device specified serial_number.
+"""Write an image file to a removable storage device specified by a serial_number as user
 
-- Uses UDisks2 and DBus to access the storage devices as user
-- Returns 0 for success, 1 for errors, and 2 for device not found or non-removable
+- Uses UDisks2 via DBus to access the storage devices without root access
 
 """
 
@@ -14,10 +13,12 @@ import dbus
 import tqdm
 
 
-def get_drives(udisk_obj):
+def get_drives(udisks_obj):
     """Prints a list of storage devices (and if they are removeable)"""
 
-    udisks_obj_manager = dbus.Interface(udisk_obj, "org.freedesktop.DBus.ObjectManager")
+    udisks_obj_manager = dbus.Interface(
+        udisks_obj, "org.freedesktop.DBus.ObjectManager"
+    )
     managed_objects = udisks_obj_manager.GetManagedObjects()
     drives = []
 
@@ -38,48 +39,69 @@ def get_drives(udisk_obj):
     return drives
 
 
-def get_removeable_drive(serial_number, udisk_obj):
-    """Returns UDisks2.Drive Interface of an external attached storage device with the specified serial number"""
-
-    udisks_obj_manager = dbus.Interface(udisk_obj, "org.freedesktop.DBus.ObjectManager")
+def get_removeable_block_device(serial_number, disk_size, udisks_obj):
+    """Returns the whole disk block device path of an external attached device with specified serial and size(can be 0)"""
+    udisks_obj_manager = dbus.Interface(
+        udisks_obj, "org.freedesktop.DBus.ObjectManager"
+    )
     managed_objects = udisks_obj_manager.GetManagedObjects()
+    drive_path = None
+    drive_size = None
 
     for path, interfaces in managed_objects.items():
         if "org.freedesktop.UDisks2.Drive" in interfaces:
             drive_info = interfaces["org.freedesktop.UDisks2.Drive"]
             if drive_info["Serial"] == serial_number and drive_info["MediaRemovable"]:
-                return path, drive_info
-    return None, None
+                if disk_size != 0 and drive_info["Size"] != disk_size:
+                    raise IndexError(
+                        "disk size error of serial {}: Should: {} Actual: {}".format(
+                            serial_number, disk_size, drive_info["Size"]
+                        )
+                    )
+                drive_path = path
+                drive_size = drive_info["Size"]
+                break
 
+    if not drive_path:
+        raise KeyError("no removable disk with serial {} found".format(serial_number))
 
-def get_block_device_path(drive_object_path, udisk_obj):
-    """Gets the block device object path."""
-
-    udisks_obj_manager = dbus.Interface(udisk_obj, "org.freedesktop.DBus.ObjectManager")
-    managed_objects = udisks_obj_manager.GetManagedObjects()
-
+    potential_block_devices = []
     for path, interfaces in managed_objects.items():
         if "org.freedesktop.UDisks2.Block" in interfaces:
-            interface = interfaces["org.freedesktop.UDisks2.Block"]
-            if interface["Drive"] == drive_object_path:
-                return path  # Return the path itself
-    return None
+            block_info = interfaces["org.freedesktop.UDisks2.Block"]
+            if "Drive" in block_info and block_info["Drive"] == drive_path:
+                potential_block_devices.append((path, block_info))
+
+    if not potential_block_devices:
+        raise KeyError(f"No block devices found for drive with serial {serial_number}")
+
+    # Prioritize the block device representing the whole disk (matching size and offset 0)
+    for path, block_info in potential_block_devices:
+        block_size = block_info.get("Size")
+        block_offset = block_info.get("Offset", 0)  # Offset might not always be present
+
+        if block_size is not None and block_size == drive_size and block_offset == 0:
+            return path, block_info
+
+    raise KeyError(
+        f"Could not find the whole disk block device for serial {serial_number}"
+    )
 
 
-def write_to_device(device_name, image_file, udisks_obj, bus):
+def write_to_device(block_device_path, image_file, bus):
     """Writes the image file to the specified device."""
 
-    # Get the block device object for the target drive
-    block_device_path = get_block_device_path(device_name, udisks_obj)
-    if not block_device_path:
-        raise Exception(f"Could not get block device path for {device_name}")
+    # Get the Block device object
     block_device_obj = bus.get_object("org.freedesktop.UDisks2", block_device_path)
+    # print(f"Block Device Object Path: {block_device_path}")
     block_device_iface = dbus.Interface(
         block_device_obj, "org.freedesktop.UDisks2.Block"
     )
+    # open target block device exclusiv for writing
     target_device_dbus_fd = block_device_iface.OpenDevice(
         "w", dbus.Dictionary({"flags": os.O_EXCL | os.O_SYNC | os.O_CLOEXEC})
     )
+    # take ownership of fd
     fd = target_device_dbus_fd.take()
 
     with open(image_file, "rb") as image_handle:
@@ -96,6 +118,7 @@ def write_to_device(device_name, image_file, udisks_obj, bus):
                 initial=current_offset,
             ) as pbar:
                 while True:
+                    # write chunk to device
                     data = image_handle.read(chunk_size)
                     if not data:
                         break
@@ -108,6 +131,12 @@ def main():
         description="Write image from to removable storage device specified by serial_number."
     )
     parser.add_argument("--dest-serial", help="Serial number of the destination drive")
+    parser.add_argument(
+        "--dest-size",
+        help="optional Byte Size of the destination drive, as additional match criteria",
+        type=int,
+        default=0,
+    )
     parser.add_argument("--source-image", help="Path to the source image file")
     group = parser.add_mutually_exclusive_group(required=False)
     group.add_argument(
@@ -132,31 +161,16 @@ def main():
             )
 
     elif args.dest_serial and args.source_image:
-        device_name, drive_info = get_removeable_drive(args.dest_serial, udisks_obj)
-        if not device_name:
-            raise KeyError("serial {} not found".format(args.dest_serial))
-        write_to_device(device_name, args.source_image, udisks_obj, bus)
+        block_device_path, block_info = get_removeable_block_device(
+            args.dest_serial, args.dest_size, udisks_obj
+        )
+        write_to_device(block_device_path, args.source_image, bus)
 
     else:
         parser.print_help()
         print("Error: Invalid arguments.", file=sys.stderr)
         sys.exit(1)
 
-
-"""
-    # 1. Loop Setup on the image file
-    udisks_manager_obj = bus.get_object(
-        "org.freedesktop.UDisks2", "/org/freedesktop/UDisks2/Manager"
-    )
-    udisks_manager = dbus.Interface(
-        udisks_manager_obj, "org.freedesktop.UDisks2.Manager"
-    )
-    loop_device = udisks_manager.LoopSetup(
-        image_handle.fileno(), dbus.Dictionary({"read-only": dbus.Boolean(True)})
-    )
-    # 4. Loop Remove
-    udisks_manager.LoopRemove(loop_device, dbus.Dictionary({}))
-"""
 
 if __name__ == "__main__":
     main()
