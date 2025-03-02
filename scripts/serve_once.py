49eee49dc8baa3f201ea349ef1fbbb1f4ece4b06
Felix Erkinger <wuxxin@gmail.com>
2025-03-02 23:20:44 +0100

    fix serve_once

--- scripts/serve_once.py
+++ scripts/serve_once.py
@@ -10,6 +10,7 @@ can be used
 calling usage: <yaml-from-STDIN> | $0 [--verbose] --yes | [<request_body-to-STDOUT>]
 
 notes:
+- named pipes are used for certificate material, no certificate data is written to disk
 - if key or cert is None, a temporary self-signed cert will be created
 - if mtls is true, ca_cert must be set, and a mandatory client certificate is needed to connect
 - if mtls_clientid is not None, the client certificate CN name needs to match mtls_clientid
@@ -25,6 +26,7 @@ import http.server
 import os
 import select
 import shutil
+import socket
 import ssl
 import sys
 import tempfile
@@ -63,10 +65,13 @@ def verbose_print(message: str) -> None:
         print(message, file=sys.stderr)
 
 
-def write_file_bytes(filepath: str, content: bytes) -> None:
-    """Writes bytes to a file."""
+def write_file_content(filepath: str, content: str | bytes) -> None:
+    """Writes bytes/str content to a file."""
     with open(filepath, "wb") as f:
-        f.write(content.encode())
+        if isinstance(content, str):
+            f.write(content.encode("utf-8"))
+        else:
+            f.write(content)
 
 
 def merge_dicts(dict1: dict, dict2: dict) -> dict:
@@ -80,7 +85,7 @@ def merge_dicts(dict1: dict, dict2: dict) -> dict:
     return merged
 
 
-def generate_self_signed_certificate(hostname: str) -> dict[str, bytes]:
+def generate_self_signed_certificate(hostname: str) -> dict[str, str]:
     """Generates a self-signed certificate for the given hostname."""
     from cryptography import x509
     from cryptography.hazmat.backends import default_backend
@@ -98,15 +103,9 @@ def generate_self_signed_certificate(hostname: str) -> dict[str, bytes]:
         .issuer_name(issuer)
         .public_key(private_key.public_key())
         .serial_number(x509.random_serial_number())
-        .not_valid_before(
-            datetime.datetime.now(datetime.UTC) - datetime.timedelta(days=1)
-        )
-        .not_valid_after(
-            datetime.datetime.now(datetime.UTC) + datetime.timedelta(days=365)
-        )
-        .add_extension(
-            x509.SubjectAlternativeName([x509.DNSName(hostname)]), critical=False
-        )
+        .not_valid_before(datetime.datetime.now(datetime.UTC) - datetime.timedelta(days=1))
+        .not_valid_after(datetime.datetime.now(datetime.UTC) + datetime.timedelta(days=365))
+        .add_extension(x509.SubjectAlternativeName([x509.DNSName(hostname)]), critical=False)
         .add_extension(
             x509.ExtendedKeyUsage(
                 [ExtendedKeyUsageOID.SERVER_AUTH, ExtendedKeyUsageOID.CLIENT_AUTH]
@@ -121,16 +120,22 @@ def generate_self_signed_certificate(hostname: str) -> dict[str, bytes]:
             encoding=serialization.Encoding.PEM,
             format=serialization.PrivateFormat.PKCS8,
             encryption_algorithm=serialization.NoEncryption(),
-        ),
-        "cert": certificate.public_bytes(serialization.Encoding.PEM),
+        ).decode("utf-8"),
+        "cert": certificate.public_bytes(serialization.Encoding.PEM).decode("utf-8"),
     }
 
 
-class RequestHandler(http.server.BaseHTTPRequestHandler):
+class OurRequestHandler(http.server.BaseHTTPRequestHandler):
     """Handles HTTP requests, serving a single response based on configuration."""
 
-    config: dict[str, Any] = {}  # Class-level variable to store configuration
-    success: bool = False
+    def __init__(self, request, client_address, server):
+        self.config = server.config
+        self._success = False
+        super().__init__(request, client_address, server)
+
+    @property
+    def success(self):
+        return self._success
 
     def verbose_error(
         self, code: int, message: str | None = None, explain: str | None = None
@@ -152,44 +157,80 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
             return subject.get("commonName")
         return None
 
-    def handle_request(self) -> None:
-        """Handles a single HTTP request based on the provided configuration."""
+    def _check_client_mtls(self) -> bool:
+        """Checks if the client certificate is valid based on the configuration"""
         if self.config["mtls"] and self.config["mtls_clientid"]:
             client_cert_cn = self.get_client_cert_common_name()
             if client_cert_cn != self.config["mtls_clientid"]:
-                self.verbose_error(
-                    401, f"Invalid Client certificate CN: {client_cert_cn}"
-                )
-                return
+                self.verbose_error(401, f"Invalid Client certificate CN: {client_cert_cn}")
+                return False
+        return True
 
-        if (
-            self.config["request_ip"]
-            and self.client_address[0] != self.config["request_ip"]
-        ):
+    def _check_client_mtls_enabled(self) -> bool:
+        if self.config["mtls"]:
+            cert = self.connection.getpeercert()
+            if not cert:
+                self.verbose_error(401, "Client certificate required")
+                return False
+        return True
+
+    def _check_client_ip(self) -> bool:
+        """Checks if the client IP is valid based on the configuration."""
+        if self.config["request_ip"] and self.client_address[0] != self.config["request_ip"]:
             self.verbose_error(403, f"Invalid Client IP : {self.client_address[0]}")
-            return
+            return False
+        return True
 
+    def _check_request_method(self) -> bool:
+        """Checks if the request method is valid based on the configuration."""
         if self.command != self.config["request_method"]:
             self.verbose_error(405, f"Invalid request method: {self.command}")
-            return
+            return False
+        return True
 
+    def _check_request_path(self) -> bool:
+        """Checks if the request path is valid based on the configuration."""
         if self.path != self.config["request_path"]:
             self.verbose_error(404, f"Invalid request path: {self.path}")
-            return
+            return False
+        return True
 
+    def _handle_request_body(self) -> None:
+        """Reads and prints the request body to stdout if enabled."""
         if self.config["request_body_stdout"]:
             content_length = int(self.headers.get("Content-Length", 0))
             request_body = self.rfile.read(content_length).decode("utf-8")
             print(request_body)
 
+    def _send_response(self) -> None:
+        """Sends the response headers and payload."""
+        payload = self.config["payload"].encode("utf-8")
         self.send_response(200)
         for key, value in self.config["header"].items():
             self.send_header(key, value)
+        self.send_header("Content-Length", str(len(payload)))
         self.end_headers()
-        self.wfile.write(self.config["payload"].encode("utf-8"))
+        self.wfile.write(payload)
+        self.wfile.flush()
+        self._success = True
 
-        # Signal successful response
-        type(self).success = True
+    def handle_request(self) -> None:
+        """Handles a single HTTP request based on the provided configuration."""
+
+        verbose_print(f"got request from {self.client_address[0]}")
+        if not self._check_client_mtls_enabled():
+            return
+        if not self._check_client_mtls():
+            return
+        if not self._check_client_ip():
+            return
+        if not self._check_request_method():
+            return
+        if not self._check_request_path():
+            return
+
+        self._handle_request_body()
+        self._send_response()
 
     def do_GET(self) -> None:
         """Handles GET requests."""
@@ -204,16 +245,23 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
         self.handle_request()
 
 
-def serve_once(config: dict[str, Any]) -> http.server.HTTPServer:
-    RequestHandler.config = config
-    RequestHandler.success = False
+def serve_once(config: dict[str, Any]) -> int:
+    """Serves a single HTTP request based on configuration."""
 
     temp_dir = None
     cert_thread = None
     key_thread = None
     httpd = None
+    serving = True
 
     try:
+        if config["serve_port"] == 0:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.bind((config["serve_ip"], 0))
+                config["serve_port"] = s.getsockname()[1]
+
+        verbose_print(f"Starting server on port {config['serve_port']}")
+
         temp_dir = tempfile.mkdtemp(dir=os.path.join("/run/user", str(os.getuid())))
         cert_fifo_path = os.path.join(temp_dir, "cert.fifo")
         key_fifo_path = os.path.join(temp_dir, "key.fifo")
@@ -221,13 +269,18 @@ def serve_once(config: dict[str, Any]) -> http.server.HTTPServer:
         os.mkfifo(key_fifo_path)
 
         cert_thread = threading.Thread(
-            target=write_file_bytes, args=(cert_fifo_path, config["cert"])
+            target=write_file_content, args=(cert_fifo_path, config["cert"])
         )
         key_thread = threading.Thread(
-            target=write_file_bytes, args=(key_fifo_path, config["key"])
+            target=write_file_content, args=(key_fifo_path, config["key"])
         )
         cert_thread.start()
         key_thread.start()
+
+        server_address = (config["serve_ip"], int(config["serve_port"]))
+        httpd = http.server.HTTPServer(server_address, OurRequestHandler)
+        httpd.config = config
+        httpd.timeout = config["timeout"]
 
         context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
         context.load_cert_chain(certfile=cert_fifo_path, keyfile=key_fifo_path)
@@ -237,35 +290,26 @@ def serve_once(config: dict[str, Any]) -> http.server.HTTPServer:
             if config["mtls"]:
                 context.verify_mode = ssl.CERT_REQUIRED
 
-        verbose_print(f"waiting for request on request_path: {config['request_path']}")
-        httpd = http.server.HTTPServer(
-            (config["serve_ip"], int(config["serve_port"])),
-            RequestHandler,
-            bind_and_activate=False,
-        )
-        httpd.timeout = config["timeout"]
         httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
-
-        httpd.server_bind()
-        httpd.server_activate()
+        verbose_print(f"waiting for request on request_path: {config['request_path']}")
 
         start_time = datetime.datetime.now()
         while (
-            not RequestHandler.success
-            and (datetime.datetime.now() - start_time).total_seconds()
-            < config["timeout"]
+            serving
+            and (datetime.datetime.now() - start_time).total_seconds() < config["timeout"]
         ):
-            try:
-                r, _, _ = select.select([httpd.socket], [], [], httpd.timeout)
-                if r:
-                    httpd.handle_request()
-            except Exception as e:
-                verbose_print(f"Request handling error: {e}")
-                # continue to listen for other requests
+            r, _, _ = select.select([httpd.socket], [], [], 1)
+            if r:
+                httpd.handle_request()
+                if httpd.RequestHandlerClass.success:
+                    serving = False
 
-        if not RequestHandler.success:
+        if serving:
             verbose_print("Timeout reached or no successful requests.")
-            sys.exit(1)
+            return 1
+
+        verbose_print("Received and served a sucessful request")
+        return 0
 
     finally:
         if cert_thread:
@@ -277,19 +321,15 @@ def serve_once(config: dict[str, Any]) -> http.server.HTTPServer:
         if httpd:
             httpd.server_close()
 
-    return httpd
 
-
-def main() -> None:
+def main() -> int:
     """Parses arguments, loads configuration, and starts the server."""
     global verbose
 
     parser = argparse.ArgumentParser(
         description=__doc__, formatter_class=argparse.RawTextHelpFormatter
     )
-    parser.add_argument(
-        "--verbose", action="store_true", help="Log and Warnings to stderr"
-    )
+    parser.add_argument("--verbose", action="store_true", help="Log and Warnings to stderr")
     parser.add_argument("--yes", action="store_true", help="Confirm execution")
 
     args = parser.parse_args()
@@ -297,11 +337,10 @@ def main() -> None:
 
     if not args.yes:
         print("Error: --yes flag is required to confirm execution")
-        sys.exit(1)
+        return 1
 
     stdin_str = sys.stdin.read()
     loaded_config = yaml.safe_load(stdin_str) if stdin_str.strip() else {}
-
     config = merge_dicts(DEFAULT_CONFIG, loaded_config)
 
     if not config["cert"] or not config["key"]:
@@ -312,10 +351,8 @@ def main() -> None:
         config["cert"] = cert_key_dict["cert"]
         config["key"] = cert_key_dict["key"]
 
-    verbose_print(f"Starting server on port {config['serve_port']}")
-    # server will exit after first request or timeout
-    serve_once(config)
+    return serve_once(config)
 
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
