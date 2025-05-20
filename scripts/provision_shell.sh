269387351cce4d9c32c3a03c797b9cdbe881ab01
Felix Erkinger <wuxxin@gmail.com>
2025-05-21 00:34:47 +0200

    cleanup

--- scripts/provision_shell.sh
+++ scripts/provision_shell.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
 set -eo pipefail
-# set -x  
+# set -x
 
-test "${1}" = "" && set -- "/usr/bin/bash" "${@:1}"; \
-podman run -it --rm \
+test "${1}" = "" && set -- "/usr/bin/bash" "${@:1}"
+${DOCKER_CMD:-podman} run -it --rm \
     --user="$(id -u):$(id -g)" --network=host \
-    -v "/etc/passwd:/etc/passwd:ro" -v "/etc/group:/etc/group:ro" \
-    -v "$HOME:$HOME" -v "$(pwd):$(pwd)" -v "$XDG_RUNTIME_DIR:$XDG_RUNTIME_DIR" \
-    -e "HOME=$HOME" -e "PWD=$(pwd)" -e "LANG=$LANG" -e "TERM=$TERM" -e "USER=$USER" \
+    -v "/etc/passwd:/etc/passwd:ro" \
+    -v "/etc/group:/etc/group:ro" \
+    -v "$HOME:$HOME" \
+    -v "$(pwd):$(pwd)" \
+    -v "$XDG_RUNTIME_DIR:$XDG_RUNTIME_DIR" \
+    -e "HOME=$HOME" \
+    -e "PWD=$(pwd)" \
+    -e "LANG=$LANG" \
+    -e "TERM=$TERM" \
+    -e "USER=$USER" \
     -e "XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR" \
     -w "$(pwd)" \
     localhost/provision-client \
