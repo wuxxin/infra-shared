7745ed7c21e1f54d5b86645a4de23b8ae5e192cb
Felix Erkinger <wuxxin@gmail.com>
2025-05-22 16:02:53 +0200

    reformat

--- scripts/shell_inside_sim.sh
+++ scripts/shell_inside_sim.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -eo pipefail
 # set -x
-current="$(sudo virsh list --id --state-running | sort |uniq | grep -v "^$" | head -n 1)"
-if test -n "$current"; then 
-    addr="$(sudo virsh guestinfo "$current"| grep "if.1.addr.0.addr" | sed -r "s/[^:]+: (.+)/\1/g" )"
+current="$(sudo virsh list --id --state-running | sort | uniq | grep -v "^$" | head -n 1)"
+if test -n "$current"; then
+    addr="$(sudo virsh guestinfo "$current" | grep "if.1.addr.0.addr" | sed -r "s/[^:]+: (.+)/\1/g")"
     ssh -A -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null "core@$addr"
 fi
