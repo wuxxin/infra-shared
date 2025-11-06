a7a4c3fd787f5d2a128dd53ed183b61fe5ffd88d
Felix Erkinger <wuxxin@gmail.com>
2025-11-06 01:26:55 +0100

    Create test_fake_ca.py

---
+++ tests/test_fake_ca.py
@@ -0,0 +1,29 @@
+import os
+import pytest
+import shutil
+from pathlib import Path
+from pulumi.automation import Stack
+
+
+def test_fake_ca_example(pulumi_stack: Stack, pulumi_project_dir, pulumi_up_args):
+    """
+    Test the fake_ca example
+    """
+
+    # Copy the __init__.py from examples/fake_ca into the pulumi_project_dir
+    shutil.copy(
+        Path("examples/fake_ca/__init__.py"),
+        pulumi_project_dir,
+    )
+
+    # Rename the copied __init__.py to __main__.py to make it the Pulumi program entry point
+    shutil.move(pulumi_project_dir / "__init__.py", pulumi_project_dir / "__main__.py")
+
+    # Run the Pulumi program
+    up_result = pulumi_stack.up(**pulumi_up_args)
+    assert up_result.summary.result == "succeeded"
+
+    # Assert on specific outputs
+    assert "fake_ca_factory" in up_result.outputs
+    assert "fake_ca_mitm_ca" in up_result.outputs
+    assert "fake_mitm_host" in up_result.outputs
